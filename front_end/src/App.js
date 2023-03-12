06ef3ef7921bef1430559badca039ef46ce253a7
homohit <91591472+homohit@users.noreply.github.com>
2023-03-12 10:29:48 -0700

    first commit

---
+++ front_end/src/App.js
@@ -0,0 +1,16 @@
+import Header from "./components/Header";
+import Home from "./components/Home.js";
+import Work from "./components/Work.js";
+import TimeLine from "./components/TimeLine";
+function App() {
+  return (<>
+  <Header/>
+  <Home/>
+  <Work/>
+  <TimeLine/>
+  
+  </>
+  );
+}
+
+export default App;
